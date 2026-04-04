12e2e13b168aeb1260b6e4cb4f1cc5500cdfaece
shifaasyedd <shifa.syed@somaiya.edu>
2026-04-05 04:35:51 +0530

    updated Google Auth

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -30,15 +30,9 @@ router.get(
 
 // 🌐 [GET] GOOGLE CALLBACK 
 // Google redirects back here after login
-router.get(
-    "/google/callback",
-    passport.authenticate("google", { session: false }),
-    (req, res) => {
-        const token = generateToken(req.user._id);
-        // Redirects to frontend with token in URL for initial storage
-        res.redirect(`http://localhost:3000/login-success?token=${token}`);
-    }
-);
+// Change the redirect line to this:
+const frontendUrl = process.env.CLIENT_URL || "http://localhost:3000";
+res.redirect(`${frontendUrl}/login-success?token=${token}`);
 
 // 📝 [POST] REGISTER 
 // Creates user and sends verification email
