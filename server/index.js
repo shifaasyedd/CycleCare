e9ceb30b9317785fff3cb278fa44299d90c952ab
shifaasyedd <shifa.syed@somaiya.edu>
2026-04-05 04:10:06 +0530

    updated CORS

--- server/index.js
+++ server/index.js
@@ -6,7 +6,10 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ["https://thecyclecare.vercel.app", "http://localhost:3000"],
+  credentials: true
+}));
 app.use(express.json());
 
 // Connect to MongoDB
